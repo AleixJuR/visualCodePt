888a2ad876d75b0414c5e388d7bdc75a24055dd9
Arnau Cabratosa <a2308739@institutmontilivi.cat>
2024-02-29 18:22:01 +0100

    Final

--- project.py
+++ project.py
@@ -2,7 +2,6 @@ import random
 #Aleix Part
 
 #Arnau Part
-#bot dirà números random intenta no escriure el mateix sinó perds.
 def Parameters():
     print("-------------------------------------------------------<()>-------------------------------------------------------")
     print("Basiclly in this game the computer will choose 2 random numbers between 1 and 20, this 2 random numbers are the parameters in which luckily your number will be in, if it's not then you lose. Good luck.")
@@ -22,7 +21,7 @@ def Parameters():
             print(f"The {num} was within the parameters, you won!! :D")
             correct = True
 
-        print(f"The parameters are {bot1, bot2}")
+        print(f"The parameters are {bot1, bot2}")#here it will be shown the parameters wich were randomly choosed.
 
 def FindTheSpot():
     print("-------------------------------------------------------<()>-------------------------------------------------------")
@@ -33,9 +32,9 @@ def FindTheSpot():
         num = int(input("Choose a number between 1 and 20 --> "))
         bot = []
         for i in range(0,15):
-            bot.append(random.randint(1,20)) # bot will choose a random number between 1 and 20, 15 times.
+            bot.append(random.randint(1,20)) # bot will choose a random number between 1 and 20, 15 times and will add them to the list.
         if num in bot:
-            print(f"The {num} is within the random numbers chosen, you lost!! :C")
+            print(f"The {num} is within the random numbers chosen, you lost!! :C")#if you write the same number computer will print the following text.
         else:
             print(f"The {num} is not within the random numbers chosen, you won!! :D")
             correct = True
